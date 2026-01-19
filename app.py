4f80af779f39f543ff8e885c460908bd5ce85616
KaiPad0 <oliverkaito@hotmail.com>
2026-01-19 11:06:17 +0900

    fix code

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask,request,send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
-def infex():
-    return send_from_directory("files", "IMG_4879.mov")
+@app.route("/files/<path:filename>")
+def infex(filename):
+    return send_from_directory("files", filename)
 
 if __name__=="__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
