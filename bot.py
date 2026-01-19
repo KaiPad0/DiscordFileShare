9092d513aaa2bca03d706c59c5485d67db167ed0
KaiPad0 <oliverkaito@hotmail.com>
2026-01-19 23:09:31 +0900

    URLミスを修正

--- bot.py
+++ bot.py
@@ -16,12 +16,13 @@ async def on_ready():
 
 @tree.command(name="upload", description="アップロード画面を開きます")
 async def upload(interaction: discord.Interaction):
+    await interaction.response.defer(ephemeral=True)
     # このコマンド専用の「後日談用URL」を取得
     # interaction.followup.url がそれにあたります
     webhook_url = interaction.followup.url
     
     # ユーザーには「ここからアップロードしてね」というリンクを送る
-    upload_url = f"http://あなたのサーバーIP:5000/upload?webhook={webhook_url}"
+    upload_url = f"http://kaipad123.servemp3.com:5000/upload?webhook={webhook_url}"
     
     await interaction.response.send_message(f"以下のリンクから動画をアップロードしてください！\n{upload_url}", ephemeral=True)
 
