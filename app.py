dcc647315216f2a31c9004a683de53513075101c
KaiPad0 <oliverkaito@hotmail.com>
2026-01-19 10:56:42 +0900

    fix bug

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def infex():
-    return send_from_directory("files", "aaa.mov")
+    return send_from_directory("files", "IMG_4879.mov")
 
 if __name__=="__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
