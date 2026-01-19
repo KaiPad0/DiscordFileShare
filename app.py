36f77f17597b797d497c6a59e6ea96b41c14a029
KaiPad0 <oliverkaito@hotmail.com>
2026-01-19 21:57:01 +0900

    add api

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 import sys
-from flask import Flask,request,send_from_directory
+import os
+from flask import Flask,request,send_from_directory,render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/files/<path:filename>")
-def infex(filename):
+def send_file(filename):
     return send_from_directory("files", filename)
 
+@app.route('/upload', methods=['GET'])
+def upload_page():
+    return render_template('upload.html')
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "ファイルがありません", 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return "ファイルが選択されていません", 400
+    if file:
+        dt = datetime.now()
+        final_filename = f"{dt.strftime("%Y%m%d%H%M%S")}.mp4"
+        final_path = os.path.join("files", final_filename)
+        file.save(final_path)
+    
+        
 if __name__=="__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
---
+++ discord.py
@@ -0,0 +1,8 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
