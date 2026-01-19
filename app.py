e0481491a657d024239cd59bc43ef6115b900466
KaiPad0 <oliverkaito@hotmail.com>
2026-01-19 11:33:00 +0900

    fix bug

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route("/files/<path:filename>")
 def infex(filename):
-    return send_from_directory("files", filename)
-
-@app.route("/")
-def aa():
-    return send_from_directory("files", aa.mp4)
+    return send_from_directory("files", filename, mimetype="video/mp4")
 
 if __name__=="__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
